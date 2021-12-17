5fbe0c54bf6b027d10bcc5a150b4c44e6828b56c
benedettagns <benedetta.genise01@gmail.com>
2021-12-17 18:33:54 +0100

    completato

--- js/script.js
+++ js/script.js
@@ -1,46 +1,56 @@
-//pulsante per scegliere la difficoltà
-
-document.addEventListener('click', function () {
-    document.querySelector('.facile').innerHTML = ('EASY');
-})
-
-//creazione griglie in base alla difficoltà
-//difficoltà 1 
-const difficoltà1 = document.querySelector('.difficoltà1');
-
-function tanteGriglie(difficoltà1) {
-    const quadrato = document.createElement('div');
-    quadrato.className = 'cella';
-    difficoltà1.append(quadrato);
+function boxColored (element) {
+    element.addEventListener('click', function () {
+        this.classList.toggle('blue');
+    })
 }
 
-for (let i = 0; i < 100; i++) {
-    tanteGriglie(difficoltà1);
-}
+document.querySelector('.facile').addEventListener('click', function () {
+    console.log('FACILE');
+    const difficoltà1 = document.querySelector('.difficoltà1');
 
-// //difficoltà 2 
-const difficoltà2 = document.querySelector('.difficoltà2');
+    function tanteGriglie(difficoltà1) {
+        const quadrato = document.createElement('div');
+        quadrato.className = 'cella';
+        difficoltà1.append(quadrato);
+        boxColored(quadrato);
+    }
 
-function griglieMedie(difficoltà2) {
-    const quadrato = document.createElement('div');
-    quadrato.className = 'cella';
-    difficoltà2.append(quadrato);
-}
+    for (let i = 0; i < 100; i++) {
+        tanteGriglie(difficoltà1);
+    }
+});
 
-for (let i = 0; i < 81; i++) {
-    griglieMedie(difficoltà2);
-}
+document.querySelector('.medio').addEventListener('click', function () {
+    console.log('MEDIO');
+    const difficoltà2 = document.querySelector('.difficoltà2');
 
-// //difficoltà 3
-const difficoltà3 = document.querySelector('.difficoltà3');
+    function griglieMedie(difficoltà2) {
+        const quadrato = document.createElement('div');
+        quadrato.className = 'cella';
+        difficoltà2.append(quadrato);
+        boxColored(quadrato);
+    }
 
-function pocheGriglie(difficoltà3) {
-    const quadrato = document.createElement('div');
-    quadrato.className = 'cella';
-    difficoltà3.append(quadrato);
-}
+    for (let i = 0; i < 81; i++) {
+        griglieMedie(difficoltà2);
+    }
+});
 
-for (let i = 0; i < 49; i++) {
-    pocheGriglie(difficoltà3);
-}
+document.querySelector('.difficile').addEventListener('click', function () {
+    console.log('DIFFICILE');
+    const difficoltà3 = document.querySelector('.difficoltà3');
+
+    function pocheGriglie(difficoltà3) {
+        const quadrato = document.createElement('div');
+        quadrato.className = 'cella';
+        difficoltà3.append(quadrato);
+        boxColored(quadrato);
+    }
+
+    for (let i = 0; i < 49; i++) {
+        pocheGriglie(difficoltà3);
+    }
+});
+
+
 
